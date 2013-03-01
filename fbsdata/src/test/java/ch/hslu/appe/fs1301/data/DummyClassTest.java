2a5bb55d6fdaec740e707fdd2254de969c2244d2
bollhals <boli89@gmail.com>
2013-03-01 15:44:55 +0100

    DummyClass to see if it builds

---
+++ fbsdata/src/test/java/ch/hslu/appe/fs1301/data/DummyClassTest.java
@@ -0,0 +1,12 @@
+package ch.hslu.appe.fs1301.data;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DummyClassTest {
+
+	@Test
+	public void GoodTest() 	{
+		assertTrue(true);
+	}
+}
