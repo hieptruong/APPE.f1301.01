b09909843489f1022b23ee723289891c2ee931a4
bollhals <boli89@gmail.com>
2013-04-11 17:09:44 +0200

    getPersonsByNames added to PersonRepository

--- fbsdata/src/main/java/ch/hslu/appe/fs1301/data/PersonRepository.java
+++ fbsdata/src/main/java/ch/hslu/appe/fs1301/data/PersonRepository.java
@@ -15,6 +15,7 @@ public class PersonRepository extends BaseRepository<Person> implements iPersonR
 		super(entityManager);		
 	}
 
+	@Override
 	public Person getPersonByUsernameAndPassword(String username, String password) {
 		StringBuilder query = new StringBuilder(100);
 		query.append("SELECT p FROM Person p WHERE p.benutzername = '");
@@ -26,5 +27,25 @@ public class PersonRepository extends BaseRepository<Person> implements iPersonR
 		List<Person> resultList = executeQuery(query);
 		
 		return resultList.isEmpty() ? null : resultList.get(0);
+	}
+
+	@Override
+	public List<Person> getPersonsByNames(String... names) {
+		if (names.length == 0)
+			throw new IllegalArgumentException("names");
+		
+		StringBuilder query = new StringBuilder(100);
+		query.append("SELECT p FROM Person p WHERE ");
+		for(String name : names) {
+			query.append("((p.vorname like '%")
+				 .append(name)
+				 .append("%') OR (p.name like '%")
+				 .append(name)
+				 .append("%')) AND ");
+		}
+		
+		query.delete(query.length()-4, query.length());
+		
+		return executeQuery(query);
 	}
 }
--- fbsdata/src/test/java/ch/hslu/appe/fs1301/data/PersonRepositoryTest.java
+++ fbsdata/src/test/java/ch/hslu/appe/fs1301/data/PersonRepositoryTest.java
@@ -1,67 +1,124 @@
 package ch.hslu.appe.fs1301.data;
 
-import static org.easymock.EasyMock.eq;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.isA;
 import static org.fest.assertions.Assertions.assertThat;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.powermock.api.easymock.PowerMock;
 
 import ch.hslu.appe.fs1301.data.shared.iAPPEEntityManager;
 import ch.hslu.appe.fs1301.data.shared.entity.Person;
 
 public class PersonRepositoryTest extends BaseTestClass {
 
+	private final static String ValidUsername = "ValidUsername";
+	private final static String DoubleUsername = "DoubleUsername";
+	private final static String ValidPassword = "ValidPassword";
+	private final static String InValidUsername = "InValidUsername";
+	private final static String InValidPassword = "InValidPassword";
+	private final static String FirstNameOne = "FirstNameOne";
+	private final static String FirstNameTwo = "FirstNameTwo";
+	private final static String LastName = "LastName";
+	
+	
 	private PersonRepository fTestee;
-	private iAPPEEntityManager fEntityManagerMock;
+	private static iAPPEEntityManager fEntityManager;
+	private static List<Person> fCreatedPersons;
+	
+	@BeforeClass
+	public static void setUpDatabase() {
+		fCreatedPersons = new ArrayList<Person>();
+		fEntityManager = new APPEEntityManager();
+		fEntityManager.startTransaction();
+		fCreatedPersons.add(createAndSavePerson(ValidUsername, ValidPassword, FirstNameOne, LastName));
+		fCreatedPersons.add(createAndSavePerson(DoubleUsername, ValidPassword, FirstNameOne, LastName));
+		fCreatedPersons.add(createAndSavePerson(DoubleUsername, ValidPassword, FirstNameTwo, FirstNameTwo));
+	}
+	
+	@AfterClass
+	public static void tearDownDatabase() {
+		//for(Person person : fCreatedPersons)
+			//fEntityManagerMock.deleteEntityObject(person);
+		fEntityManager.rollbackTransaction();
+	}
 	
 	@Override
 	public void setUp() {
-		fEntityManagerMock = PowerMock.createMock(iAPPEEntityManager.class);
-		fTestee = new PersonRepository(fEntityManagerMock);
+		fTestee = new PersonRepository(fEntityManager);
 	}
 	
 	@Test
 	public void returnsNull_WhenQueryReturnsNoPersons() {
-		setupReturnValueOfQuery();
-		PowerMock.replayAll();
-		
-		Person returnedPerson = fTestee.getPersonByUsernameAndPassword("username", "password");
+		Person returnedPerson = fTestee.getPersonByUsernameAndPassword(InValidUsername, InValidPassword);
 		
 		assertThat(returnedPerson).isNull();
 	}
 	
 	@Test
 	public void returnsPerson_WhenQueryReturnsOnePerson() {
-		Person expectedPerson = new Person();
-		setupReturnValueOfQuery(expectedPerson);
-		PowerMock.replayAll();
+		Person returnedPerson = fTestee.getPersonByUsernameAndPassword(ValidUsername, ValidPassword);
 		
-		Person returnedPerson = fTestee.getPersonByUsernameAndPassword("username", "password");
-		
-		assertThat(returnedPerson).isSameAs(expectedPerson);
+		assertThat(returnedPerson.getBenutzername()).isSameAs(ValidUsername);
+		assertThat(returnedPerson.getPasswort()).isSameAs(ValidPassword);
 	}
 	
 	@Test
 	public void returnsFirstPerson_WhenQueryReturnsMultiplePersons() {
-		Person expectedPerson = new Person();
-		Person notExpectedPerson = new Person();
-		setupReturnValueOfQuery(expectedPerson, notExpectedPerson);
-		PowerMock.replayAll();
+		Person returnedPerson = fTestee.getPersonByUsernameAndPassword(DoubleUsername, ValidPassword);
 		
-		Person returnedPerson = fTestee.getPersonByUsernameAndPassword("username", "password");
-		
-		assertThat(returnedPerson).isSameAs(expectedPerson);
-		assertThat(returnedPerson).isNotSameAs(notExpectedPerson);
+		assertThat(returnedPerson.getBenutzername()).isSameAs(DoubleUsername);
+		assertThat(returnedPerson.getPasswort()).isSameAs(ValidPassword);
 	}
 	
-	private void setupReturnValueOfQuery(Person... persons) {
-		ArrayList<Person> ExpectedReturnValue = new ArrayList<Person>();
-		for(Person person : persons)
-			ExpectedReturnValue.add(person);
-		expect(fEntityManagerMock.executeQuery(isA(String.class), eq(Person.class))).andReturn(ExpectedReturnValue);
+	@Test
+	public void returnsPersons_WhenSearchWithOneName(){
+		List<Person> returnedPersons = fTestee.getPersonsByNames(FirstNameOne);
+		
+		assertThat(returnedPersons).contains(fCreatedPersons.get(0), fCreatedPersons.get(1));
+	}
+	
+	@Test
+	public void returnsPersons_WhenSearchWithMultipleNames() {
+		List<Person> returnedPersons = fTestee.getPersonsByNames(FirstNameOne, LastName);
+		
+		assertThat(returnedPersons).contains(fCreatedPersons.get(0), fCreatedPersons.get(1));
+	}
+	
+	@Test
+	public void returnsNoPersons_WhenSearchWithNotExistingName(){
+		List<Person> returnedPersons = fTestee.getPersonsByNames(InValidUsername);
+		
+		assertThat(returnedPersons).isEmpty();
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void throwsException_WhenSearchWithNoName() {
+		fTestee.getPersonsByNames();
+	}
+	
+	private static Person createAndSavePerson(String username, String password, String firstname, String lastname) {
+		Person person = new Person();
+		person.setBenutzername(username);
+		person.setPasswort(password);
+		person.setVorname(firstname);
+		person.setName(lastname);
+		
+		setIrrelevantValues(person);
+		fEntityManager.persist(person);
+		return person;
+	}
+	
+	private static void setIrrelevantValues(Person person) {
+		person.setEMail("Test");
+		person.setOrt("Test");
+		person.setPlz(1);
+		person.setGeburtstag(new Date());
+		person.setRolle(0);
+		person.setStrasse("Test");
+		person.setAktiv(false);
 	}
 }
