e61d3176469e1770233c425272a36d32f5a21138
bollhals <boli89@gmail.com>
2013-05-03 14:30:26 +0200

    Added first part of CurrencyFieldTest

---
+++ fbsguiclient/src/test/java/ch/hslu/appe/fs1301/gui/controls/CurrencyFieldTest.java
@@ -0,0 +1,49 @@
+package ch.hslu.appe.fs1301.gui.controls;
+
+import org.junit.Test;
+
+import ch.hslu.appe.fs1303.gui.controls.APPECurrencyField;
+import ch.hslu.appe.fs1303.gui.controls.APPETextControl;
+
+public class CurrencyFieldTest extends FieldTestBase<APPECurrencyField, Integer> {
+
+	@Test
+	public void TestBaseClass() {
+		RunBaseClassTest(APPETextControl.class);
+	}
+	
+	@Test
+	public void TestGetFieldClass() {
+		RunFieldClassTest(Integer.class);
+	}
+	
+	@Test
+	public void TestGetValueForModel_WhenStringIsEmpty() {
+		RunGetValueForModelTest("", null);
+	}
+	
+	@Test
+	public void TestGetValueForModel_WhenStringIsNull() {
+		RunGetValueForModelTest(null, null);
+	}
+	
+	@Test
+	public void TestGetValueForModel_WhenStringIsNumeric() {
+		RunGetValueForModelTest("12.3", 1230);
+	}
+	
+	@Test(expected = NumberFormatException.class)
+	public void TestGetValueForModel_WhenStringIsNotNumeric() {
+		RunGetValueForModelTest("12d3", null);
+	}
+	
+	@Test
+	public void TestGetDisplayValue_WhenValueIsNull() {
+		RunGetDisplayValueTest(null, "");
+	}
+	
+	@Test
+	public void TestGetDisplayValue_WhenValueIsNumeric() {
+		RunGetDisplayValueTest(1234, "12.34");
+	}
+}
